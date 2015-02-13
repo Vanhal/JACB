86a9ac814e67fa775f125da9299e63fcccc90c19
Vanhal <obriensh@gmail.com>
2015-02-13 15:47:48 +0000

    Fixed Dupe Bug

--- src/main/java/tv/vanhal/jacb/BlockBench.java
+++ src/main/java/tv/vanhal/jacb/BlockBench.java
@@ -20,7 +20,7 @@ import net.minecraft.world.World;
 
 public class BlockBench extends BlockContainer {
 	public final String blockName = "betterBench";
-	
+
 	@SideOnly(Side.CLIENT)
 	protected IIcon top;
 	@SideOnly(Side.CLIENT)
@@ -37,67 +37,67 @@ public class BlockBench extends BlockContainer {
 	public TileEntity createNewTileEntity(World world, int p_149915_2_) {
 		return new TileBench();
 	}
-	
+
 	public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player, int side, float hitX, float hitY, float hitZ) {
 		if (!world.isRemote) {
 			FMLNetworkHandler.openGui(player, JACB.instance, 1, world, x, y, z);
 		}
 		return true;
 	}
-	
+
 	@Override
 	public void breakBlock(World world, int x, int y, int z, Block block, int p_149749_6_) {
 		TileBench tileEntity = (TileBench)world.getTileEntity(x, y, z);
 
         if (tileEntity != null) {
             ArrayList<ItemStack> items = getInsides(world, x, y, z);
-            
+
             for (ItemStack item: items) {
             	dumpItems(world, x, y, z, item);
             }
-            
+
 
             world.func_147453_f(x, y, z, block);
         }
         super.breakBlock(world, x, y, z, block, p_149749_6_);
     }
-	
+
 	public void dumpItems(World world, int x, int y, int z, ItemStack items) {
 		EntityItem entItem = new EntityItem(world, (float)x + 0.5f, (float)y + 0.5f, (float)z + 0.5f, items);
 		float f3 = 0.05F;
 		entItem.motionX = (double)((float)world.rand.nextGaussian() * f3);
 		entItem.motionY = (double)((float)world.rand.nextGaussian() * f3 + 0.2F);
 		entItem.motionZ = (double)((float)world.rand.nextGaussian() * f3);
-		
+
 		if (items.hasTagCompound()) {
 			entItem.getEntityItem().setTagCompound((NBTTagCompound)items.getTagCompound().copy());
         }
-		
+
 		world.spawnEntityInWorld(entItem);
 	}
-	
+
 	protected ArrayList<ItemStack> getInsides(World world, int x, int y, int z) {
 		ArrayList<ItemStack> items = new ArrayList<ItemStack>();
-		
+
 		TileBench tileEntity = (TileBench)world.getTileEntity(x, y, z);
 		if (tileEntity != null) {
         	//get the inventory
-            for (int i = 0; i < tileEntity.getSizeInventory(); ++i) {
+            for (int i = 0; i < tileEntity.getSizeInventory() - 1; ++i) {
                 ItemStack itemstack = tileEntity.getStackInSlot(i);
                 if (itemstack != null) {
                 	items.add(itemstack);
                 }
             }
 		}
-		
+
 		return items;
 	}
-	
+
 	@SideOnly(Side.CLIENT)
     public IIcon getIcon(int side, int meta) {
         return side == 1 ? this.top : Blocks.planks.getBlockTextureFromSide(side);//(side == 0 ? Blocks.planks.getBlockTextureFromSide(side) : (side != 2 && side != 4 ? this.blockIcon : this.front));
     }
-	
+
 	@SideOnly(Side.CLIENT)
     public void registerBlockIcons(IIconRegister register) {
 		String iconPrefix = "minecraft:crafting_table";
